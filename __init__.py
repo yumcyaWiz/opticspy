e5fa2f94b745f686b4048ab4716acc0366d590e1
yumcyawiz <hashdolf1923@gmail.com>
2020-09-16 07:36:36 +0900

    fix module not found error

--- __init__.py
+++ __init__.py
@@ -9,6 +9,10 @@
  *  Copyright (c) 2014-2015 Xing fan
 """
 from __future__ import division as __division__
+
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "."))
+
 import aperture, interferometer_seidel,interferometer_zenike
 import seidel, seidel2, zernike, test, tools, diffraction, jones, gauss
 import phaseunwrap, lens, asphere, mplot3d, zernike_rec
